6417f46b7f76ddb5adb802bd329816f9b10889f1
Adam Massoud <adam.massoud@finbourne.com>
2024-01-04 10:55:48 +0000

    CTECH-3410: Don't set app name header in api client when app name is null.

--- tests/Tests/Unit/ApiFactoryTest.cs
+++ tests/Tests/Unit/ApiFactoryTest.cs
@@ -1,3 +1,4 @@
+using Lusid.Sdk.Api;
 using NUnit.Framework;
 using System;
 
@@ -31,6 +32,33 @@ namespace Finbourne.Sdk.Extensions.Tests.Unit
             Assert.That(
                 () => new ApiFactory(apiConfig),
                 Throws.InstanceOf<UriFormatException>().With.Message.EqualTo("Invalid Uri: xyz"));
+        }
+
+        [Test]
+        public void NullAppName_NoLusidApplicationHeaderInApiConfig()
+        {
+            var apiConfig = new ApiConfiguration()
+            {
+                PersonalAccessToken = "test token",
+                BaseUrl = "https://example.lusid.com/api"
+            };
+            var apiFactory = new ApiFactory(apiConfig);
+            Assert.That(apiFactory.Api<ApplicationMetadataApi>().Configuration.DefaultHeaders, Does.Not.ContainKey("X-LUSID-Application"));
+        }
+
+        [Test]
+        public void AppNameProvided_LusidApplicationHeaderInApiConfig()
+        {
+            var appName = "test app name";
+            var apiConfig = new ApiConfiguration()
+            {
+                PersonalAccessToken = "test token",
+                BaseUrl = "https://example.lusid.com/api",
+                ApplicationName = appName
+            };
+            var apiFactory = new ApiFactory(apiConfig);
+            Assert.That(apiFactory.Api<ApplicationMetadataApi>().Configuration.DefaultHeaders, Does.ContainKey("X-LUSID-Application"));
+            Assert.AreEqual(appName, apiFactory.Api<ApplicationMetadataApi>().Configuration.DefaultHeaders["X-LUSID-Application"]);
         }
     }
 }
