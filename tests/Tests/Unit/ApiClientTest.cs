84cb2cbeea3e3a40106e17c54a9a96219c5a6b2b
Adam Massoud <adam.massoud@finbourne.com>
2024-03-22 14:32:16 +0000

    CTECH-3411: Add additional encoding test. Remove unused request.

--- tests/Tests/Unit/ApiClientTest.cs
+++ tests/Tests/Unit/ApiClientTest.cs
@@ -8,6 +8,7 @@ using Moq.Protected;
 using System.Net;
 using RestSharp;
 using System.Collections.Generic;
+using System.Text;
 
 namespace Finbourne.Sdk.Extensions.Tests.Unit
 {
@@ -46,12 +47,44 @@ namespace Finbourne.Sdk.Extensions.Tests.Unit
                 }
             };
             var res = apiClient.Get<String>("/{arg}", requestOptions);
-
-            var req = new RestRequest(new Uri("http://example.com/{arg}"));
-            
             Assert.AreEqual(HttpStatusCode.OK, res.StatusCode);
             Assert.AreEqual("/prefix-%252F-suffix", res.Content);
         }
-        
+        /// <summary>
+        /// Test to ensure several different strings are encoded correctly
+        /// when sent as url segment parameters.
+        /// </summary>
+        [Test]
+        public void UrlSegmentParameterEncodings(){
+            var handler = new Mock<HttpMessageHandler>(MockBehavior.Strict);
+            handler.Protected().Setup<Task<HttpResponseMessage>>(
+                    "SendAsync", 
+                    ItExpr.IsAny<HttpRequestMessage>(), 
+                    ItExpr.IsAny<CancellationToken>())
+                .ReturnsAsync((HttpRequestMessage req, CancellationToken ct) => new HttpResponseMessage()
+                {
+                    StatusCode = HttpStatusCode.OK,
+                    Content = new StringContent(req.RequestUri!.AbsolutePath ?? "oops")
+                });
+            Func<RestClientOptions, HttpMessageHandler> HttpClientFactoryMock = (options) => handler.Object;
+            var apiClient = new ApiClient("http://example.com", CreateHttpMessageHandler : HttpClientFactoryMock);
+            var requestOptions = new RequestOptions(){
+                Operation = "GET",
+                PathParameters=new Dictionary<string, string>(){
+                    {"plus", "prefix-+-suffix"},
+                    {"space", "prefix- -suffix"},
+                    {"percent24", "prefix-%24-suffix"}
+                }
+            };
+            var res = apiClient.Get<String>("/{plus}/{space}/{percent24}", requestOptions);
+            var expectedPath = new StringBuilder()
+                .Append("/prefix-%2B-suffix")
+                .Append("/prefix-%20-suffix")
+                .Append("/prefix-%2524-suffix")
+                .ToString();
+            
+            Assert.AreEqual(HttpStatusCode.OK, res.StatusCode);
+            Assert.AreEqual(expectedPath, res.Content);
+        }
     }
 }
